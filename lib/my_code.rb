1a2cac2d04fc8ac83c06d1e321b0c7b902dfdd9f
Christine Bumpous <bumpouce@gmail.com>
2020-01-28 02:20:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,9 +3,10 @@ def map_to_negative (array)
   i = 0 
 
   while i < array.length do
-    negative_array << [0 - array[i]]
+    negative_array << [-array[i]]
     i += 1
   end
-
+  
+  pp "negative_array: #{negative_array}"
   negative_array
 end
