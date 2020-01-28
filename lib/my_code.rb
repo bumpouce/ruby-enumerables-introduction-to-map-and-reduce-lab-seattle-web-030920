2b931056d50026b57264a53417529bc5f1556092
Christine Bumpous <bumpouce@gmail.com>
2020-01-28 02:23:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,10 +3,8 @@ def map_to_negativize (array)
   i = 0 
   negative_array = []
   
-  pp "Given input: #{array}"
-
   while i < array.length do
-    negative_array << [array[i]]
+    negative_array << array[i]* -1
     i += 1
   end
   
